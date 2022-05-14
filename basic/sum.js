b46eae8005ad2ac2348021154fdd722cfd72026a
Mandy <qqamandy@gmail.com>
2022-05-14 15:12:01 +0800

    callback1

---
+++ basic/sum.js
@@ -0,0 +1,26 @@
+// function sum(n){
+//     let result=0;
+//     for(let i = 1; i <= n; i++){
+//         result += i;
+//     }
+//     return result;
+// }
+
+// console.log(sum(1));
+// console.log(sum(2));
+// console.log(sum(10));
+// console.log(sum(100));
+
+function sum(n){
+    let arr =[];
+    for(let i = 1; i <= n; i++){
+        arr.push(i);
+    }
+    // console.log(arr);
+    arr.reduce(function(acc, item){
+        return
+    }, 0 )
+}
+
+sum(10);
+
