b41d51a5e5841f7cff3042e3e276a6d58d08adfe
Mandy <qqamandy@gmail.com>
2022-06-07 20:28:22 +0800

    stocks pagination unfinished

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,65 +1,101 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mysql = require('mysql2');
-const cors = require('cors');
+const path = require("path");
+const mysql = require("mysql2");
+const cors = require("cors");
 app.use(cors());
 
-require('dotenv').config();
+require("dotenv").config();
 
-let pool = mysql.createPool({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DB,
-    connectionLimit:10,
-
-}).promise();
+let pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DB,
+    connectionLimit: 10,
+  })
+  .promise();
 
 //5/21ㄉ部分 製作可切換ㄉ網址
-app.get('/', (request, response, next) => {
-    response.send("Home");
+app.get("/", (request, response, next) => {
+  response.send("Home");
 });
 
-app.get('/about', (request, response, next) => {
-    response.send("About me");
-})
-
-app.listen(3001, () => {
-    console.log('start server at port 3000');
-})
+app.get("/about", (request, response, next) => {
+  response.send("About me");
+});
 
 //RESTful API
 //取得stock列表
-app.get('/stocks', async (req, res, next) => {
-    let [data, fields] = await pool.execute('SELECT * FROM stocks');
-    res.json(data);
+app.get("/stocks", async (req, res, next) => {
+  let [data, fields] = await pool.execute("SELECT * FROM stocks");
+  res.json(data);
+});
+
+// 取得某個股票 id 的資料
+app.get("/stocks/:stockId", async (req, res, next) => {
+  // 取得網址上的參數 req.params
+  // req.params.stockId
+  console.log("get stocks by id", req.params);
+  // let [data, fields] = await pool.execute(
+  //   "SELECT * FROM stock_prices WHERE stock_id = ?",
+  //   [req.params.stockId]
+  // );
+  // console.log("query stock by id", data);
+
+  //1. 取得目前在第幾頁
+  let page = req.query.page || 1;
+  console.log("current page = ", page);
+
+  //2. 取得目前總筆數
+  let [allResults, fields] = await pool.execute(
+    "SELECT * FROM stock_prices WHERE stock_id = ?",
+    [req.params.stockId]
+  );
+  const total = allResults.length;
+  console.log("total = ", total);
+
+  //3. 計算總共有幾筆
+  const perPage = 5; //每頁有幾筆
+  const lastPage = Math.ceil(total / perPage);
+  console.log("last page = ", lastPage);
+
+  //計算offset要跳過幾筆
+  let offset = (page - 1) * perPage;
+  console.log("offset = ", offset);
+
+  //
+  let [pageResults] = await pool.execute(
+    "SELECT * FROM stock_prices WHERE stock_id = ? ORDER BY date DESC LIMIT ? OFFSET ?",
+    [req.params.stockId, perPage, offset]
+  );
+
+  res.json({
+    //儲存頁碼ㄉ位置
+    pagination: {
+      total,
+      lastPage,
+      page,
+      //其實應該要寫成
+      // total: total,
+      // lastPage: lastPage,
+      // page: page
+    },
+    //真正ㄉdata
+    data: pageResults,
   });
-  
-  // 取得某個股票 id 的資料
-  app.get('/stocks/:stockId', async (req, res, next) => {
-    // 取得網址上的參數 req.params
-    // req.params.stockId
-    console.log('get stocks by id', req.params);
-    let [data, fields] = await pool.execute('SELECT * FROM stocks WHERE id = ' + req.params.stockId);
-  
-    console.log('query stock by id:', data);
-    // 空資料(查不到資料)有兩種處理方式：
-    // 1. 200OK 就回 []
-    // 2. 回覆 404
-    if (data.length === 0) {
-      // 這裡是 404 範例
-      res.status(404).json(data);
-    } else {
-      res.json(data);
-    }
-  });
-  
-  // 這個中間件在所有路由的後面
-  // 會到這裡，表示前面所有的路由中間件都沒有比到符合的網址
-  // => 404
-  app.use((req, res, next) => {
-    console.log('所有路由的後面 ==> 404', req.path);
-    res.status(404).send('Not Found');
-  });
+});
+
+// 這個中間件在所有路由的後面
+// 會到這裡，表示前面所有的路由中間件都沒有比到符合的網址
+// => 404
+app.use((req, res, next) => {
+  console.log("所有路由的後面 ==> 404", req.path);
+  res.status(404).send("Not Found");
+});
+
+app.listen(3001, () => {
+  console.log("start server at port 3001");
+});
--- simple-express/stock-fe/src/components/StockDetails.js
+++ simple-express/stock-fe/src/components/StockDetails.js
@@ -1,17 +1,59 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const StockDetails = () => {
+  const [data, setData] = useState([]);
+  //目前在第幾頁
+  const [page, setPage] = useState(1);
+  //總筆數
+  const [lastPage, setLastPage] = useState(1);
+
+  useEffect(() => {
+    let getPrice = async () => {
+      let response = await axios.get("http://localhost:3001/stocks/2330");
+      setData(response.data.data);
+    };
+    getPrice();
+  }, []);
+
   return (
     <div>
-      <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交金額：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交股數：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">開盤價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">收盤價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">漲跌價差：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">最高價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">最低價：</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交筆數：</h2>
-      </div>
+      {data.map((item) => {
+        return (
+          <div
+            key={item.date}
+            className="p-6 m-6 bg-white rounded-lg shadow bg-gray-50"
+          >
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              日期：{item.date}
+            </h2>
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              成交金額：{item.amount}
+            </h2>
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              成交股數：{item.volume}
+            </h2>
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              開盤價：{item.open_price}
+            </h2>
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              收盤價：{item.close_price}
+            </h2>
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              漲跌價差：{item.delta_price}
+            </h2>
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              最高價：{item.high_price}
+            </h2>
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              最低價：{item.low_price}
+            </h2>
+            <h2 className="mb-2 text-2xl font-bold text-gray-800">
+              成交筆數：{item.transactions}
+            </h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
